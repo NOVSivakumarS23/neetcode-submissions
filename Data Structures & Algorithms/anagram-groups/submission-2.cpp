92e85180c05020f2a751e8fe2f3c2391eec8217b
sriram sivakumar <sriramsvm@gmail.com>
2026-09-17 13:38:57 -0700

    Add: anagram-groups - submission-2

---
+++ Data Structures & Algorithms/anagram-groups/submission-2.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map<string, vector<string>> mp;
+
+        for (string& s : strs) {
+            int count[26] = {0};
+            for (char c : s) {
+                count[c - 'a']++;
+            }
+            
+            // Build a unique key, e.g., "#1#0#2..." based on character frequencies
+            string key = "";
+            for (int i = 0; i < 26; i++) {
+                key += "#" + to_string(count[i]);
+            }
+            
+            mp[key].push_back(s);
+        }
+
+        vector<vector<string>> result;
+        for (auto& pair : mp) {
+            result.push_back(move(pair.second));
+        }
+
+        return result;
+    }
+};
